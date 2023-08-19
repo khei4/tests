806204214e59d835cf86b748796bf36562d01653
khei4 <kk.asano.luxy@gmail.com>
2023-08-19 17:15:24 +0900

    save entry, mention, per user

--- rss_feeder_discord/rss_feeder.py
+++ rss_feeder_discord/rss_feeder.py
@@ -3,22 +3,28 @@ from discord.ext import commands, tasks
 import feedparser
 import os
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-GUILD_ID = os.getenv('DISCORD_GUILD_ID')
-CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
 
 if not TOKEN:
     raise ValueError("DISCORD_BOT_TOKEN is not set in the .env file")
-if not GUILD_ID:
-    raise ValueError("DISCORD_GUILD_ID is not set in the .env file")
-if not CHANNEL_ID:
-    raise ValueError("DISCORD_CHANNEL_ID is not set in the .env file")
+# TODO: show help
+# TIP: https://rss.app
 
-GUILD_ID = int(GUILD_ID)
-CHANNEL_ID = int(CHANNEL_ID)
+def init_db():
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS feeds
+                 (user_id INTEGER, channel_id INTEGER, feed_url TEXT, latest_entry TEXT)''')
+
+    conn.commit()
+    conn.close()
+
+init_db()
 
 # Intentsの設定
 intents = discord.Intents.default()
@@ -37,65 +43,97 @@ async def on_ready():
 
 @bot.command()
 async def addfeed(ctx, url: str):
-    # TODO: save on sqlite3? user info, channel info
-    print("addfeed called")
-    if url not in rss_feeds:
-        rss_feeds.append(url)
-        await ctx.send(f"RSS feed {url} has been added!")
-    else:
-        await ctx.send(f"RSS feed {url} is already in the list!")
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
+    
+    # Check if feed already exists for this user
+    c.execute("SELECT * FROM feeds WHERE user_id=? AND feed_url=?", (ctx.author.id, url))
+    if c.fetchone():
+        await ctx.send("This feed is already added.")
+        return
+
+    c.execute("INSERT INTO feeds (user_id, channel_id, feed_url, latest_entry) VALUES (?, ?, ?, ?)",
+              (ctx.author.id, ctx.channel.id, url, ""))
+
+    conn.commit()
+    conn.close()
+
+    await ctx.send(f"RSS feed {url} has been added to channel {ctx.channel.name}!")
 
 @bot.command()
 async def removefeed(ctx, url: str):
-    if url in rss_feeds:
-        rss_feeds.remove(url)
-        latest_entries.pop(url, None)  # remove the latest entry for this feed if it exists
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
+    
+    # Check if feed exists for this user
+    c.execute("SELECT * FROM feeds WHERE user_id=? AND feed_url=?", (ctx.author.id, url))
+    if c.fetchone():
+        c.execute("DELETE FROM feeds WHERE user_id=? AND feed_url=?", (ctx.author.id, url))
+        conn.commit()
         await ctx.send(f"RSS feed {url} has been removed!")
     else:
         await ctx.send(f"RSS feed {url} is not in the list!")
 
+    conn.close()
+
+@bot.command()
+async def resetfeed(ctx, member: discord.Member):
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
+    
+    # Delete all feeds for the mentioned user
+    c.execute("DELETE FROM feeds WHERE user_id=?", (member.id,))
+    conn.commit()
+    conn.close()
+
+    await ctx.send(f"All RSS feeds for {member.mention} have been removed!")
+
 @bot.command()
 async def debugfeed(ctx):
     await ctx.send(f"chu!")
-    for feed_url in rss_feeds:
-        feed = feedparser.parse(feed_url)
-        print(json.dumps(feed, indent=2))
-        entry = feed.entries[0]
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
 
-        if latest_entries.get(feed_url) == entry.link:
-            continue
+    c.execute("SELECT * FROM feeds")
+    feeds = c.fetchall()
 
-        guild = discord.utils.get(bot.guilds, id=GUILD_ID)
-        if not guild:
-            continue
+    for feed in feeds:
+        user_id, channel_id, feed_url, latest_entry = feed
+        fetched_feed = feedparser.parse(feed_url)
 
-        channel = guild.get_channel(CHANNEL_ID)
-        if not channel:
-            continue
+        if fetched_feed.entries and fetched_feed.entries[0].link != latest_entry:
+            channel = bot.get_channel(channel_id)
+            user = await bot.fetch_user(user_id)
+            await channel.send(f"{user.mention} **{fetched_feed.entries[0].title}**\n{fetched_feed.entries[0].link}")
 
-        await channel.send(f"**{entry.title}**\n{entry.link}")
-        latest_entries[feed_url] = entry.link
+            c.execute("UPDATE feeds SET latest_entry=? WHERE user_id=? AND feed_url=?", 
+                      (fetched_feed.entries[0].link, user_id, feed_url))
 
-import json
+    conn.commit()
+    conn.close()
+
 @tasks.loop(minutes=10)
 async def check_feed():
-    for feed_url in rss_feeds:
-        feed = feedparser.parse(feed_url)
-        print(json.dumps(feed, indent=2))
-        entry = feed.entries[0]
+    conn = sqlite3.connect('rss_feeds.db')
+    c = conn.cursor()
 
-        if latest_entries.get(feed_url) == entry.link:
-            continue
+    c.execute("SELECT * FROM feeds")
+    feeds = c.fetchall()
 
-        guild = discord.utils.get(bot.guilds, id=GUILD_ID)
-        if not guild:
-            continue
+    for feed in feeds:
+        user_id, channel_id, feed_url, latest_entry = feed
+        fetched_feed = feedparser.parse(feed_url)
 
-        channel = guild.get_channel(CHANNEL_ID)
-        if not channel:
-            continue
+        if fetched_feed.entries and fetched_feed.entries[0].link != latest_entry:
+            channel = bot.get_channel(channel_id)
+            user = await bot.fetch_user(user_id)
+            await channel.send(f"{user.mention} **{fetched_feed.entries[0].title}**\n{fetched_feed.entries[0].link}")
 
-        await channel.send(f"**{entry.title}**\n{entry.link}")
-        latest_entries[feed_url] = entry.link
+            c.execute("UPDATE feeds SET latest_entry=? WHERE user_id=? AND feed_url=?", 
+                      (fetched_feed.entries[0].link, user_id, feed_url))
+
+    conn.commit()
+    conn.close()
+
 
 bot.run(TOKEN)
